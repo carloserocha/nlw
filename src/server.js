4c060dabfe0c2b8411d97550783326c159a4827e
nicole-taufenbach <ntaufenbach@hotmail.com>
2020-08-06 17:48:50 -0300

    Aula 4 da NLW finalizada :)

---
+++ src/server.js
@@ -0,0 +1,108 @@
+//npm run dev
+const proffys = [
+    {
+       name: "Diego Fernandes",
+       avatar: "https://github.com/diego3g.png", 
+       whatsapp: "900000000", 
+       bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+       subject: "Química", 
+       cost: "20", 
+       weekday: [0], 
+       time_from: [720], 
+       time_to: [1200]
+     },
+     {
+       name: "Mayk Brito",
+       avatar: "https://github.com/maykbrito.png", 
+       whatsapp: "900000000", 
+       bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+       subject: "Química", 
+       cost: "20", 
+       weekday: [1], 
+       time_from: [720], 
+       time_to: [1200]
+     },
+     {
+        name: "Bob",
+        avatar: "https://pbs.twimg.com/profile_images/1257072318490316801/xlxGy0Hi_400x400.jpg", 
+        whatsapp: "900000000", 
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1200]
+      }
+   ]
+
+const subjects = [
+  "Artes",
+  "Biologia",
+  "Ciências",
+  "Educação física",
+  "Física",
+  "Geografia",
+  "História",
+  "Matemática",
+  "Portugês",
+  "Química"
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+
+function getSubjects(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+  }
+
+function pageStudy(req, res) {
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
+  }
+
+function pageGiveClasses(req, res) {
+  const data = req.query
+
+  const isNotEmpty = Object.keys(data).length != 0
+
+  if(isNotEmpty) {
+
+    data.subject = getSubjects(data.subject)
+    proffys.push(data)
+    
+    return res.redirect("/study")
+  }
+
+  return res.render("give-classes.html", { subjects, weekdays })
+}
+
+const express = require('express')
+const server = express()
+
+//config nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+//configurar arquivos estáticos (css, scripts, imagens)
+server.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+
+.listen(5500);
